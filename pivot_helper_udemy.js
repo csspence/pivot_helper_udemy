120a3b177e472acd2f5cac33415d421db6f7471e
Crew Spence <csspence123@gmail.com>
2020-08-05 05:46:44 -0500

    Wrote working solution.

--- pivot_helper_udemy.js
+++ pivot_helper_udemy.js
@@ -5,5 +5,20 @@ Create a function pivotHelper that accepts an array and makes the first element 
 */
 
 const pivotHelper = (arr) => {
-  
+  let index = 0;
+  for(let i = 1; i < arr.length; i++) {
+    if(arr[i] < arr[0]) {
+      index++;
+      let temp = arr[i];
+      arr[i] = arr[index];
+      arr[index] = temp;
+    }
+  }
+  if(index > 0) {
+    let holder = arr[0];
+    arr[0] = arr[index];
+    arr[index] = holder;
+  }
+  console.log('here is arr: ' + arr);
+  return index;
 }
