e7ae20e593829305d57c6e3a9809ce3c7c2cf2bc
Crew Spence <csspence123@gmail.com>
2020-08-05 05:34:25 -0500

    Created function stub for toy problem.

---
+++ pivot_helper_udemy.js
@@ -0,0 +1,9 @@
+/*
+JavaScript Algorithms and Data Structures Masterclass
+Pivot Helper
+Create a function pivotHelper that accepts an array and makes the first element of that array a pivot.  It should arrange the array (in place) such that the numbers to the left of the pivot are less than the pivot and so the numbers to the right of the pivot are greater than the pivot.  pivotHelper returns the final index of the pivot.
+*/
+
+const pivotHelper = (arr) => {
+  
+}
